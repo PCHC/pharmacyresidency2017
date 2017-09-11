828eb02add4c35400bbc876c921eab0e2fb4a262
Chris Violette <cvioletterun@gmail.com>
2017-09-11 09:30:01 -0400

    Adding logo, updating header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,16 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="{{ home_url('/') }}">
+        <img src="@asset('images/pchc-logo.svg')" class="d-inline-block">
+        <span>{{ get_bloginfo('name', 'display') }}</span>
+      </a>
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        <div class="collapse navbar-collapse" id="navbarNav">
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav ml-auto']) !!}
+        </div>
       @endif
     </nav>
-  </div>
 </header>
