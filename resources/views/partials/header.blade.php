0ce6c186e01f25c890307a78b84ba5d2b40455d2
Chris Violette <cvioletterun@gmail.com>
2017-09-12 16:14:37 -0400

    Header styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,11 @@
 <header class="banner">
-    <nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="{{ home_url('/') }}">
-        <img src="@asset('images/pchc-logo.svg')" class="d-inline-block">
-        <span>{{ get_bloginfo('name', 'display') }}</span>
-      </a>
+    <a class="brand" href="{{ home_url('/') }}">
+      <img src="@asset('images/pchc-logo.svg')" class="d-inline-block">
+      <span>{{ App::siteName() }}</span>
+    </a>
+    <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
-        <div class="collapse navbar-collapse" id="navbarNav">
-          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav ml-auto']) !!}
-        </div>
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
 </header>
