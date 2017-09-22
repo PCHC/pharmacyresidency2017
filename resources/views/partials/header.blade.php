92c444438c2414513710e6d48dbfa2ec8e16ff1d
Chris Violette <cviolette@L5159.local>
2017-09-22 16:08:00 -0400

    Adding media queries to make things mobile-friendly

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,20 @@
       <img src="@asset('images/pchc-logo.svg')" class="d-inline-block">
       <span>{{ App::siteName() }}</span>
     </a>
-    <nav class="nav-primary">
+    <form role="search" method="get" class="search-form" action="{{ home_url('/') }}">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for..." aria-label="Search for..."  name="s">
+        <span class="input-group-btn">
+          <button class="btn btn-primary" type="submit">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+        </span>
+      </div>
+    </form>
+    <button class="nav-toggle hidden-md-up" data-toggle="collapse" data-target=".nav-primary">
+      <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
+    </button>
+    <nav class="nav-primary collapse">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
