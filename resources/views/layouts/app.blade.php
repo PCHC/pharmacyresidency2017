cf7dbc303cb48d3f56cf139f51e027ae504fefbd
Chris Violette <cvioletterun@gmail.com>
2017-09-27 16:17:31 -0400

    Rewrapped the content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,12 @@
 <html @php(language_attributes())>
   @include('partials.head')
   <body @php(body_class())>
-    <div class="grid-wrapper__main">
-    @php(do_action('get_header'))
-    @include('partials.header')
+    <div class="grid-wrapper grid-wrapper--header">
+      @php(do_action('get_header'))
+      @include('partials.header')
+    </div>
     @include('partials.widgets-front-page')
+    <div class="grid-wrapper grid-wrapper--main">
     <main class="main" role="document">
       @yield('content')
     </main>
@@ -15,8 +17,10 @@
       </aside>
     @endif
     </div>
+    <div class="grid-wrapper grid-wrapper--footer">
     @php(do_action('get_footer'))
     @include('partials.footer')
+    </div>
     @php(wp_footer())
   </body>
 </html>
--- resources/views/partials/widgets-front-page.blade.php
+++ resources/views/partials/widgets-front-page.blade.php
@@ -1,5 +1,7 @@
 @if( is_front_page() )
-  <div class="widgets-front-page">
-    @php(dynamic_sidebar('widgets-front-page'))
+  <div class="grid-wrapper grid-wrapper--widgets-front-page">
+    <div class="widgets-front-page">
+      @php(dynamic_sidebar('widgets-front-page'))
+    </div>
   </div>
 @endif
