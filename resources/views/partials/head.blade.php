e34fa30e197d96c08fcfcde647f351bba05fa5c6
Chris Violette <cvioletterun@gmail.com>
2017-09-28 14:43:48 -0400

    Added favicons

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" href="{{ App\asset_path('assets/images/favicon.png') }}">
+  <link rel="icon" sizes="192x192" href="{{ App\asset_path('assets/images/favicon-highres.png') }}">
   @php(wp_head())
 </head>
